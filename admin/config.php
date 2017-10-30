99817fa983024a40600651edd31f3dab39714b06
Erwin Alan Frías Martínez <alan.frias@erwcode.com>
2017-10-30 13:15:24 -0600

    Directory folders
    Adding folders to contain the blog's files.
    It's just the first step.

---
+++ admin/config.php
@@ -0,0 +1,21 @@
+<?php 
+
+	define('RUTA', 'http://localhost/blog/');
+
+	$bd_config = array(
+		'db' => 'blog',
+		'user' => 'root',
+		'pass' => ''
+	);
+
+	$blog_config = array(
+		'post_por_pagina' => '2',
+		'images_folder' => 'images/'
+	);
+
+	$blog_admin = array(
+		'user' => 'Admin',
+		'pass' => '123'
+	);
+
+?>
